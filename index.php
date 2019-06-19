5717d13bd175d174d7fac5b44f02c50353ad99fa
omaedelen9478 <52007597+omaedelen9478@users.noreply.github.com>
2019-06-19 12:38:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.uswitchforbusiness.com/business-energy/compare-business-energy-broker");
 die();
 ?>
